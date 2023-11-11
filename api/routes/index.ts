71c87b1a89ae1a05c3134627c7eb6d190a96f4c4
qraxiss <qraxiss@gmail.com>
2023-11-11 16:09:10 +0300

    Sample model deleted!

--- api/routes/index.ts
+++ api/routes/index.ts
@@ -1,11 +1,9 @@
 import { historyRouter } from './history'
-import { sampleRouter } from './sample'
 import { authRouter } from './auth'
 
 import { Express } from 'express'
 
 export function addRoutes(app: Express) {
     app.use('/auth', authRouter)
-    app.use('/sample', sampleRouter)
     app.use('/history', historyRouter)
 }
